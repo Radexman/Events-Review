f42376997d3e7e997e3fef07984606340db3056a
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-03-22 18:37:05 +0100

    22.03

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-// ============= 06 Input Events ============= //
+// =================== 01 Event Listeners ================== //
+
+// const clearBtn = document.querySelector('#clear');
+
+// const onClear = () => {
+// 	alert('Clear items');
+// };
+
+// JavaScript Event Listener
+
+// clearBtn.onclick = function () {
+// 	alert('Clear Items');
+// };
+
+// addEventListener()
+
+// clearBtn.addEventListener('click', clearBtn);
+// clearBtn.addEventListener('click', () => console.log('Clear Itmes'));
+
+// setTimeout(() => clearBtn.removeEventListener('click', onClear), 5000);
+
+// setTimeout(() => onClear.click(), 5000);
+
+// const clearButton = document.querySelector('#clear');
+// const items = document.querySelectorAll('li');
+
+// const clearItems = () => {
+// 	items.forEach((item) => item.remove());
+// };
+
+// clearButton.addEventListener('click', clearItems);
